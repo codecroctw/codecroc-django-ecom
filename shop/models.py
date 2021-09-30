fe8ffe224a9edadeb1a636aca134eaedf43ea3da
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-30 19:26:20 +0800

    demo product model, admin

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=127)
+    description = models.TextField(blank=True, null=True)
