870d3bbb5aeebccd24385a8f13767fc2ecb341af
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-10-02 16:35:01 +0800

    category, product model

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=63)
+    image = models.ImageField(null=True, default=None)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=127)
     description = models.TextField(blank=True, null=True)
+    primary_image = models.ImageField(null=True, default=None)
+    original_price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0.00)
+    discounted_price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0.00)
+    category = models.ForeignKey(
+        Category, blank=True, null=True, on_delete=models.SET_NULL)
