6ab4e43a6cc6fd6d196b905d18d4df9ca8a0e131
oliver <oliver@pop-os.localdomain>
2021-10-07 12:22:37 +0800

    order model, customer fk

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-from uuid import uuid4
 from django.utils import timezone
+from django.conf import settings
+
+from uuid import uuid4
+
+User = settings.AUTH_USER_MODEL # auth.User
 
 
 def path_and_rename(instance, filename):
@@ -40,3 +44,12 @@ class Product(models.Model):
     class Meta:
         verbose_name = '產品'
         verbose_name_plural = '產品'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='客戶')
+    created_at = models.DateTimeField('建立於', auto_now_add=True)
+    updated_at = models.DateTimeField('更新於', auto_now=True)
+    total = models.DecimalField(
+        '特價', max_digits=6, decimal_places=2, default=0.00)
+    status = models.CharField('訂單狀態', max_length=63, null=True, blank=True)
