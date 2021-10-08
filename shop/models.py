2cf81a7a363dc032e975952ff33d28d479270b9f
oliver <oliver@pop-os.localdomain>
2021-10-08 11:34:43 +0800

    status text choices

--- shop/models.py
+++ shop/models.py
@@ -47,13 +47,23 @@ class Product(models.Model):
 
 
 class Order(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        ORDER_SENT = 'order_sent', '已下單'
+        PAID = 'paid', '已付款'
+        INVOICE_MADE = 'invoce_made', '已開立發票'
+        PRODUCT_SENT = 'product_sent', '已寄出'
+        ORDER_CLOSED = 'order_closed', '訂單結束'
+        ORDER_FAILED = 'order_failed', '訂單失敗'
+
     customer = models.ForeignKey(
         User, on_delete=models.CASCADE, verbose_name='客戶')
     created_at = models.DateTimeField('建立於', auto_now_add=True)
     updated_at = models.DateTimeField('更新於', auto_now=True)
     total = models.DecimalField(
         '特價', max_digits=6, decimal_places=2, default=0.00)
-    status = models.CharField('訂單狀態', max_length=63, null=True, blank=True)
+    status = models.CharField(
+        '訂單狀態', max_length=63, choices=StatusChoices.choices, default=StatusChoices.ORDER_SENT)
     products = models.ManyToManyField(
         Product, verbose_name='訂單內容', related_name='orders', through='Mapping')
 
