3fe17d5e6fd123fd4f5a50057b2919d3a05243aa
oliver <oliver@pop-os.localdomain>
2021-10-11 13:16:22 +0800

    django-admin-interface

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # admin interface (3rd)
+    'admin_interface',
+    'colorfield',
+
+    # django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # user apps
     'shop',
 ]
