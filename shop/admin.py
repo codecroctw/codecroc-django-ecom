c7435a3ac0a334bd8349b4ded0eaf98d34713319
oliver <oliver@pop-os.localdomain>
2021-10-17 16:43:14 +0800

    shop admin list customization

--- shop/admin.py
+++ shop/admin.py
@@ -4,9 +4,27 @@ from .models import Product, Category, Order, Mapping
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
+# admin.site.register(Product)
+# admin.site.register(Order)
 admin.site.register(Mapping)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category',
+                    'original_price', 'discounted_price']
+    search_fields = ['title', 'description', ]
+    list_filter = ['category', ]
+    list_display_links = ['id', 'title', ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'status',
+                    'created_at', 'updated_at', 'total']
+    list_editable = ['status', ]
+    list_filter = ['status', 'created_at', 'updated_at', ]
+    search_fields = ['name', 'phone', 'admin', ]
 
 
 class ShopAdminSite(admin.AdminSite):
