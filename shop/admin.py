124b93d20185fb743ef82a40b779f65c0f802a3e
oliver <oliver@pop-os.localdomain>
2021-10-24 20:44:45 +0800

    order admin action

--- shop/admin.py
+++ shop/admin.py
@@ -39,6 +39,12 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ['name', 'phone', 'admin', ]
     inlines = [OrderProductInline, ]
 
+    actions = ['make_order_invoice']
+
+    @admin.action(description='開立發票')
+    def make_order_invoice(modeladmin, request, queryset):
+        queryset.update(status=Order.StatusChoices.INVOICE_MADE)
+
 
 class ShopAdminSite(admin.AdminSite):
     site_header = '商品後台'
