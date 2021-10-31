1ae0b80e5ec006c1a4feab9da7dd613531e2b1d6
oliver <oliver@pop-os.localdomain>
2021-10-31 17:52:59 +0800

    login redirect, auth

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from shop.admin import shop_admin
@@ -8,6 +8,7 @@ from shop.views import home
 
 urlpatterns = [
     path('', home),
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('shop_admin/', shop_admin.urls)
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='/auth/login')
 def home(req):
     # return HttpResponse('<h1>Hello World</h1>')
+
     return render(req, template_name='test.html')
