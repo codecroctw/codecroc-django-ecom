89065a333ee1dea54461604c62ba6e38c1b2b95f
oliver <oliver@pop-os.localdomain>
2022-02-26 20:54:26 +0800

    basic form validation

--- crm/forms.py
+++ crm/forms.py
@@ -2,4 +2,23 @@ from django import forms
 
 
 class TestForm(forms.Form):
-	q = forms.CharField(label="搜尋")
+	some_text = forms.CharField(required=False)
+	boolean = forms.BooleanField(required=False)
+	integer = forms.IntegerField()
+	email = forms.EmailField(required=False)
+
+	def clean_integer(self, *args, **kwargs):
+		'''
+		clean_[field_name]
+		integer 必須 >= 10
+  		'''
+		integer = self.cleaned_data.get('integer')
+		if integer < 10:
+			raise forms.ValidationError('integer 必須 >= 10')
+		return integer
+
+	def clean_some_text(self, *args, **kwargs):
+		some_text = self.cleaned_data.get('some_text')
+		if len(some_text) < 10:
+			raise forms.ValidationError('Some Text 的長度需 >= 10')
+		return some_text
--- crm/views.py
+++ crm/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from .forms import TestForm
 
 def search_view(req):
-    form = TestForm()
-    if req.method == 'GET':
-        print(req.GET)
-    if req.method == 'POST':
-        print(req.POST)
+    form = TestForm(req.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    #if req.method == 'GET':
+    #    print(req.GET)
+    #if req.method == 'POST':
+    #    print(req.POST)
     return render(req, 'search-form.html', {'form': form})
